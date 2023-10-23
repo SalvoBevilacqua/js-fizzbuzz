1de2890ab185ae5231f95906e465612ae863cb7a
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-10-23 10:30:42 +0200

    fix:script

--- script.js
+++ script.js
@@ -9,22 +9,20 @@ for (let i = 1; i <= 100; i++) {
 
     if(i % 5 === 0 && i % 3 === 0) {
         result = "fizzbuzz";
-        console.log(result);
-        gridString += `<div class="square ${result}">FizzBuzz</div>`
 
     } else  if(i % 5 === 0) {
         result = "buzz";
-        console.log(result);
-        gridString += `<div class="square ${result}">Buzz</div>`
 
     } else if(i % 3 === 0) {
         result = "fizz";
-        console.log(result);
-        gridString += `<div class="square ${result}">Fizz</div>`
 
     } else {
-        gridString += `<div class="square">${i}</div>`
+        result = i;
     }
+
+    gridString += `<div class="square ${result}">${result}</div>`
+
+    console.log(result);
     
 }
 
